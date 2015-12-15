a75313d320add1e99d82dc317ea30df1fc301d67
jgb <txoppe@gmail.com>
2015-12-15 20:35:54 +0100

    Add .editorconfig file

--- ejercicio-cookie-crear.php
+++ ejercicio-cookie-crear.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // crear una cookie que vaya aumentando, cuando las visitas lelguen hasta 10 el valor se reinicia
 if (isset($_COOKIE['contadorVisitas']) && $_COOKIE['contadorVisitas'] <= 9) {
   setcookie('contadorVisitas', $_COOKIE['contadorVisitas'] + 1, time() + 3600);
